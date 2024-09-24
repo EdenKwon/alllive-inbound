3445880b1dec3a047ff058c63b4383bfc1d8598f
EdenKwon <ybinn@caresquare.kr>
2024-09-24 18:56:50 +0900

    feat : 첫 레포지토리 세팅
    첫 레포지토리 세팅 완료

---
+++ src/main/java/allliveyoung/allliveinbound/AllliveInboundApplication.java
@@ -0,0 +1,13 @@
+package allliveyoung.allliveinbound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllliveInboundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllliveInboundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/allliveyoung/allliveinbound/AllliveInboundApplicationTests.java
@@ -0,0 +1,13 @@
+package allliveyoung.allliveinbound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllliveInboundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
