a3898d2f84db8b174b6c40c7fcdb60cdb1011be7
EdenKwon <ybinn@caresquare.kr>
2024-09-27 16:37:40 +0900

    refactor : 로직 변경으로 인한 입고 검수 삭제 및 전반적 코드 수정

--- src/main/java/allliveyoung/allliveinbound/web/controller/InboundInspectionController.java
+++ src/main/java/allliveyoung/allliveinbound/web/controller/InboundInspectionController.java
@@ -1,6 +1,5 @@
 package allliveyoung.allliveinbound.web.controller;
 
-import allliveyoung.allliveinbound.service.InboundInspectionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
--- src/main/java/allliveyoung/allliveinbound/web/dto/InboundRequestSaveDTO.java
+++ src/main/java/allliveyoung/allliveinbound/web/dto/InboundRequestSaveDTO.java
@@ -1,6 +1,6 @@
 package allliveyoung.allliveinbound.web.dto;
 
-import allliveyoung.allliveinbound.domain.Company;
+import allliveyoung.allliveinbound.domain.Member;
 import allliveyoung.allliveinbound.domain.Warehouse;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,6 +14,6 @@ import lombok.NoArgsConstructor;
 public class InboundRequestSaveDTO {
     private Long id;
     private String code;
-    private Company company;
+    private Member member;
     private Warehouse warehouse;
 }
